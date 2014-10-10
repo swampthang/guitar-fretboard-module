84623ec0be131085958f7e0c85ab7fbd55c64512
Carvel Avis <cavis@rhapsody.com>
2014-10-10 09:48:06 -0500

    Incorporate vextab into the chord module.

--- js/vertical.neckmodule.js
+++ js/vertical.neckmodule.js
@@ -19,7 +19,9 @@ var chordModule = (function() {
     stringDivs:               "",
     naturals:                 ('A','B','C','D','E','F','G'),
     accidentals:              ('A#','Bb','C#','Db','D#','Eb','F#','Gb','G#','Ab'),
-    viewMode:                 "arpeggio",
+    viewMode:                 "chord",
+    notationData:             "",
+    renderer:                 {},
     strings: {
       1:  new Array('E','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb','E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb'),
       2:  new Array('B,Cb','B#,C','C#,Db','D','D#,Eb','E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#','D','D#','E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb'),
@@ -28,40 +30,48 @@ var chordModule = (function() {
       5:  new Array('A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb','E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb','E,Fb','E#,F','F#,Gb','G','G#,Ab'),
       6:  new Array('E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb','E,Fb','E#,F','F#,Gb','G','G#,Ab','A','A#,Bb','B,Cb','B#,C','C#,Db','D','D#,Eb')
     },
-    scales: {
+    majorScales: {
       'C':   new Array('C','D','E','F','G','A','B','Eb','Bb','Gb','G#','Db','D#','F#'),
-      'G':   new Array('G','A','B','C','D','E','F#','Bb','F','Db','D#','Ab','A#','C#'),
-      'D':   new Array('D','E','F#','G','A','B','C#','F','C','Ab','A#','Eb','E#','G#'),
-      'A':   new Array('A','B','C#','D','E','F#','G#','C','G','Eb','E#','Bb','B#','D#'),
-      'E':   new Array('E','F#','G#','A','B','C#','D#','G','D','Bb','B#','F','G','A#'),
-      'B':   new Array('B','C#','D#','E','F#','G#','A#','D','A','F','G','C','D','E#'),
-      'F#':  new Array('F#','G#','A#','B','C#','D#','E#','A','E','C','D','G','A','B#'),
-      'C#':  new Array('C#','D#','E#','F#','G#','A#','B#','E','B','G','A','D','E','G'),
       'F':   new Array('F','G','A','Bb','C','D','E','Ab','Eb','Cb','C#','Gb','G#','B'),
       'Bb':  new Array('Bb','C','D','Eb','F','G','A','Db','Ab','Fb','F#','Cb','C#','E'),
       'Eb':  new Array('Eb','F','G','Ab','Bb','C','D','Gb','Db','A','B','Fb','F#','A'),
       'Ab':  new Array('Ab','Bb','C','Db','Eb','F','G','Cb','Gb','D','E','A','B','D'),
       'Db':  new Array('Db','Eb','F','Gb','Ab','Bb','C','Fb','Cb','G','A','D','E','G'),
+      'C#':  new Array('C#','D#','E#','F#','G#','A#','B#','E','B','G','A','D','E','G'),
       'Gb':  new Array('Gb','Ab','Bb','Cb','Db','Eb','F','Bb','Fb','C','D','G','A','C'),
-      'Cb':  new Array('Cb','Db','Eb','Fb','Gb','Ab','Bb','E','Bb','F','G','C','D','F')
+      'F#':  new Array('F#','G#','A#','B','C#','D#','E#','A','E','C','D','G','A','B#'),
+      'Cb':  new Array('Cb','Db','Eb','Fb','Gb','Ab','Bb','E','Bb','F','G','C','D','F'),
+      'B':   new Array('B','C#','D#','E','F#','G#','A#','D','A','F','G','C','D','E#'),
+      'E':   new Array('E','F#','G#','A','B','C#','D#','G','D','Bb','C','F','G','A#'),
+      'A':   new Array('A','B','C#','D','E','F#','G#','C','G','Eb','E#','Bb','B#','D#'),
+      'D':   new Array('D','E','F#','G','A','B','C#','F','C','Ab','A#','Eb','E#','G#'),
+      'G':   new Array('G','A','B','C','D','E','F#','Bb','F','Db','D#','Ab','A#','C#')
     },
     // after major scale notes come altered notes - (7=>m3), (8=>b7), (9=>b5), (10=>#5), (11=>b9), (12=>#9), (13=>#11)
     chordTypes: {
-      'major': {'symbol': '', 'selectName': 'Major Triad', 'arrayPositions': [0,2,4], 'intervals': [1,3,5]},
-      'minor': {'symbol': 'm', 'selectName': 'Minor Triad', 'arrayPositions': [0,7,4], 'intervals': [1,3,5]},
-      'diminished': {'symbol': 'd', 'selectName': 'Diminished Triad', 'arrayPositions': [0,7,9], 'intervals': [1,3,5]},
-      'maj7': {'symbol': 'maj7', 'selectName': 'Major 7th', 'arrayPositions': [0,2,4,6], 'intervals': [1,3,5,7]},
-      'min7': {'symbol': 'm7', 'selectName': 'Minor 7th', 'arrayPositions': [0,7,4,8], 'intervals': [1,3,5,7]},
-      'dom7': {'symbol': '7', 'selectName': 'Dominant 7th', 'arrayPositions': [0,2,4,8], 'intervals': [1,3,5,7]},
-      'min7b5': {'symbol': 'm7b5', 'selectName': 'Minor 7b5', 'arrayPositions': [0,7,9,8], 'intervals': [1,3,5,7]},
-      'dim7': {'symbol': 'd7', 'selectName': 'Diminished 7th', 'arrayPositions': [0,7,9,5], 'intervals': [1,3,5,7]},
-      'maj9': {'symbol': 'maj9', 'selectName': 'Major 9th', 'arrayPositions': [0,2,4,6,1], 'intervals': [1,3,5,7]},
-      'min9': {'symbol': 'm9', 'selectName': 'Minor 9th', 'arrayPositions': [0,7,4,8,1], 'intervals': [1,3,5,7]},
-      'dom9': {'symbol': '9', 'selectName': 'Dominant 9th', 'arrayPositions': [0,2,4,8,1], 'intervals': [1,3,5,7]},
-      'dom7b5': {'symbol': '7b5', 'selectName': 'Dominant 7b5', 'arrayPositions': [0,2,9,8], 'intervals': [1,3,5,7]},
-      'dom7b5b9': {'symbol': '7b5b9', 'selectName': 'Dominant 7b5b9', 'arrayPositions': [0,2,9,8,11], 'intervals': [1,3,5,7]},
-      'dom7#5': {'symbol': '7#5', 'selectName': 'Dominant 7#5', 'arrayPositions': [0,2,10,8], 'intervals': [1,3,5,7]},
-      'dom7#5b9': {'symbol': '7#5b9', 'selectName': 'Dominant 7#5b9', 'arrayPositions': [0,2,10,8,11], 'intervals': [1,3,5,7]},
+      'major': {'symbol': '', 'selectName': 'Major Triad', 'arrayPositions': [0,2,4], 'intervals': [1,3,5], 'basicType': 'major', 'scaleInterval': 1},
+      'minor': {'symbol': 'm', 'selectName': 'Minor Triad', 'arrayPositions': [0,7,4], 'intervals': [1,3,5], 'basicType': 'minor', 'scaleInterval': 1},
+      'diminished': {'symbol': 'd', 'selectName': 'Diminished Triad', 'arrayPositions': [0,7,9], 'intervals': [1,3,5], 'basicType': 'diminished', 'scaleInterval': 7},
+      'maj7': {'symbol': 'maj7', 'selectName': 'Major 7th', 'arrayPositions': [0,2,4,6], 'intervals': [1,3,5,7], 'basicType': 'major', 'scaleInterval': 1},
+      'min7': {'symbol': 'm7', 'selectName': 'Minor 7th', 'arrayPositions': [0,7,4,8], 'intervals': [1,3,5,7], 'basicType': 'minor', 'scaleInterval': 6},
+      'dom7': {'symbol': '7', 'selectName': 'Dominant 7th', 'arrayPositions': [0,2,4,8], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 5},
+      'min7b5': {'symbol': 'm7b5', 'selectName': 'Minor 7b5', 'arrayPositions': [0,7,9,8], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 7},
+      'dim7': {'symbol': 'd7', 'selectName': 'Diminished 7th', 'arrayPositions': [0,7,9,5], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'maj9': {'symbol': 'maj9', 'selectName': 'Major 9th', 'arrayPositions': [0,2,4,6,1], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'min9': {'symbol': 'm9', 'selectName': 'Minor 9th', 'arrayPositions': [0,7,4,8,1], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'dom9': {'symbol': '9', 'selectName': 'Dominant 9th', 'arrayPositions': [0,2,4,8,1], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'dom7b5': {'symbol': '7b5', 'selectName': 'Dominant 7b5', 'arrayPositions': [0,2,9,8], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'dom7b5b9': {'symbol': '7b5b9', 'selectName': 'Dominant 7b5b9', 'arrayPositions': [0,2,9,8,11], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'dom7#5': {'symbol': '7#5', 'selectName': 'Dominant 7#5', 'arrayPositions': [0,2,10,8], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+      'dom7#5b9': {'symbol': '7#5b9', 'selectName': 'Dominant 7#5b9', 'arrayPositions': [0,2,10,8,11], 'intervals': [1,3,5,7], 'basicType': '', 'scaleInterval': 1},
+    },
+    noteOctaves: {
+      1: new Array(5,8,20),
+      2: new Array(4,1,13),
+      3: new Array(4,5,17),
+      4: new Array(4,10,22),
+      5: new Array(3,3,15),
+      6: new Array(3,8,20)
     },
     setChordNotes: function(root,type) {
 
@@ -70,7 +80,7 @@ var chordModule = (function() {
       root = this.rootnote;
       type = this.chordtype;
 
-      this.scaleArr = this.scales[root];
+      this.scaleArr = this.majorScales[root];
       this.typeArr = this.chordTypes[type];
       this.intervalArr = this.typeArr['intervals'];
       this.positionArr = this.typeArr['arrayPositions'];
@@ -87,6 +97,16 @@ var chordModule = (function() {
     }
 
   };
+
+  chord.getKey = function() {
+    var root = this.rootnote;
+    var type = this.chordtype;
+    var chordRoot = this.scale[root];
+    var symbol = this.getSymbol;
+    // switch(type) {
+    //   case ''
+    // }
+  }
 
   chord.preserveMetaData = function() {
 
@@ -201,10 +221,6 @@ var chordModule = (function() {
     return selectors;
   };
 
-  chord.buildViewModeControls = function() {
-    //  stopped here
-  }
-
   chord.buildChord = function(root,type) {
     var chordNotes = this.setChordNotes(root,type);
     this.wrapper.find('.note').hide().removeClass('in-chord on off').removeAttr('interval');
@@ -237,7 +253,7 @@ var chordModule = (function() {
         toggleStringVisibility:     true,
         neckStyleClass:             "",
         position:                   0,
-        viewMode:                   "arpeggio"
+        viewMode:                   "chord"
       };
 
       if(arguments !== undefined) {
@@ -329,6 +345,12 @@ var chordModule = (function() {
     // MAIN MODULE WRAPPER
     this.htmlPieces += openMainWrapper();
 
+    this.htmlPieces += openChordControlsDiv();
+    this.htmlPieces += this.buildPositionSelectors(this.position);
+    this.htmlPieces += this.buildChordTypeSelectors(this.chordtype);
+    this.htmlPieces += this.buildRootNoteSelectors(this.rootnote);
+    this.htmlPieces += closeDiv();
+
     this.htmlPieces += this.buildMaintitleDiv();
 
     // TOP
@@ -342,10 +364,7 @@ var chordModule = (function() {
 
     // close mainModuleWrapper
     this.htmlPieces += closeDiv();
-    this.htmlPieces += this.buildPositionSelectors(this.position);
-    this.htmlPieces += this.buildChordTypeSelectors(this.chordtype);
-    this.htmlPieces += this.buildRootNoteSelectors(this.rootnote);
-
+    this.htmlPieces += buildVexTabContainer();
     container.html(this.htmlPieces);
 
     return this.htmlPieces;
@@ -415,6 +434,62 @@ var chordModule = (function() {
     this.wrapper.find('.note.in-chord').addClass('pulse');
   }
 
+  chord.processNoteName = function(noteName) {
+
+    if(noteName.indexOf('#') !== -1) {
+      return noteName;
+    }
+    if(noteName.indexOf('b') !== -1) {
+      return noteName.replace('b','@');
+    }
+    return "";
+  }
+
+  chord.findOctave = function(string,fret) {
+    /* 
+    1: new Array(5,8,20),
+    2: new Array(4,1,13),
+    3: new Array(4,5,17),
+    4: new Array(4,10,22),
+    5: new Array(3,3,15),
+    6: new Array(3,8,20)
+    */
+    if(fret >= this.noteOctaves[string][2]) {
+      // eg, string 5 fret 16
+      return this.noteOctaves[string][0] + 2;
+    }
+    if(fret >= this.noteOctaves[string][1]) {
+      // eg, string 2 fret 13
+      return this.noteOctaves[string][0] + 1;
+    }
+    return this.noteOctaves[string][0];
+  }
+
+  chord.getFretString = function(note,which) {
+    var id = note.attr('id');
+    var positionArray = id.substr(2).split("_");
+    if(which == "string") {
+      return positionArray[0];
+    } else {
+      return positionArray[1];
+    }
+  }
+
+  chord.initVexFlow = function() {
+    Vex.Flow.Artist.DEBUG = true;
+    Vex.Flow.VexTab.DEBUG = true;
+    var notationContainer = this.container.find('#chord-notation')[0];
+    this.renderer = new Vex.Flow.Renderer(notationContainer,
+      Vex.Flow.Renderer.Backends.CANVAS);
+
+    this.container.find('#notation-data').change(function(){
+      console.log('i changed');
+      this.renderNotation();
+    });
+
+    this.renderNotation();
+  }
+
   chord.stackChordNotation = function(notes) {
 
     var lastItem = notes[1].length-1;
@@ -426,35 +501,33 @@ var chordModule = (function() {
 
       for (var n=0; n<notes[s].length; n++) {
 
-        var id = notes[s][n].attr('id');
-        var positionArray = id.substr(2).split("_");
-
+        var fret = this.getFretString(notes[s][n], 'fret');
+        var string = this.getFretString(notes[s][n], 'string');
+        var noteName = this.processNoteName(notes[s][n].attr('notename'));
+        var octave = this.findOctave(string,fret);
+        if(noteName !== "") {
+          noteName += octave+"_";
+        }
         if(notes[s][n] != lastEl) {
 
           if(n==0) {
 
-            chordStack += positionArray[1] + "/" + positionArray[0] + ".";
-
+            chordStack += noteName + fret + "/" + string + ".";
           } else {
 
             notes[s][n].addClass('off').removeClass('on');
-
           }
 
         } else {
           console.log('on lastEl');
           if(n==0) {
 
-            chordStack += positionArray[1] + "/" + positionArray[0] + ")";
-          
+            chordStack += noteName + fret + "/" + string + ")";
           } else {
 
             notes[s][n].addClass('off').removeClass('on');
-
           }
-
         }
-
       }
     }
     // doublecheck to be sure the string isn't gonna break
@@ -468,15 +541,21 @@ var chordModule = (function() {
     var arpeggioNotes = "";
     for(var s=6; s>=1; s--) {
       for (var n=0; n<notes[s].length; n++) {
-        var id = notes[s][n].attr('id');
-        var positionArray = id.substr(2).split("_");
-        arpeggioNotes += positionArray[1] + "/" + positionArray[0] + " ";
+        var fret = this.getFretString(notes[s][n], 'fret');
+        var string = this.getFretString(notes[s][n], 'string');
+        var noteName = this.processNoteName(notes[s][n].attr('notename'));
+        var octave = this.findOctave(string,fret);
+        if(noteName !== "") {
+          noteName += octave+"_";
+        }
+        arpeggioNotes += noteName + fret + "/" + string + " ";
       }
     }
     return arpeggioNotes;
   }
 
   chord.createNotation = function() {
+    this.initVexFlow();
     var vextab = "options space=20\n tabstave notation=true\n ";
     vextab += "notes ";
     var notesOnString = this.notesInView();
@@ -487,7 +566,23 @@ var chordModule = (function() {
     }
     
     notationData = vextab;
-    render();
+    var viewWidth = (this.viewMode == "arpeggio") ? 400 : 250;
+    this.renderNotation(viewWidth);
+  }
+
+  chord.renderNotation = function(width) {
+    artist = new Vex.Flow.Artist(10, 10, width, {scale: 0.8});
+    vextab = new Vex.Flow.VexTab(artist);
+    try {
+      vextab.reset();
+      artist.reset();
+      vextab.parse(notationData);
+      artist.render(this.renderer);
+      $("#error").text("");
+    } catch (e) {
+      console.log(e);
+      $("#error").html(e.message.replace(/[\n]/g, '<br/>'));
+    }
   }
 
   chord.switchPosition = function(pos) {
@@ -521,10 +616,18 @@ var chordModule = (function() {
     return positionClassName;
   };
 
+  var buildVexTabContainer = function() {
+    return '<div class="vextab-container">\n <canvas id="chord-notation"></canvas>\n <div id="error"></div>\n</div>';
+  }
+
   chord.buildMaintitleDiv = function() {
     var symbol = this.getSymbol(this.chordtype);
     return '<h2 class="chord-name">'+this.rootnote+((symbol != "") ? '<sup>'+symbol+'</sup>' : "")+'</h2>';
   };
+
+  var openChordControlsDiv = function() {
+    return "<div class='chord-controls'>";
+  }
 
   var openMainWrapper = function() {
     return '<div class="vertical-neck-module-wrapper">';
@@ -553,3 +656,37 @@ var chordModule = (function() {
   return chord;
 
 })();
+
+// var notationData = "options space=20\n tabstave notation=true\n notes :w (0/6.3/6.0/3.1/2.0/1.0/1)";
+// var render;
+// var $ = jQuery;
+
+// Vex.Flow.Artist.DEBUG = true;
+// Vex.Flow.VexTab.DEBUG = true;
+
+// renderer = new Vex.Flow.Renderer($('#chord-notation')[0],
+//   Vex.Flow.Renderer.Backends.CANVAS);
+
+// $('#notation-data').change(function(){
+//   console.log('i changed');
+//   this.renderNotation();
+// });
+
+// this.renderNotation();
+
+// render = function(width) {
+//   artist = new Vex.Flow.Artist(10, 10, width, {scale: 0.8});
+//   vextab = new Vex.Flow.VexTab(artist);
+//   try {
+//     vextab.reset();
+//     artist.reset();
+//     vextab.parse(notationData);
+//     artist.render(renderer);
+//     $("#error").text("");
+//   } catch (e) {
+//     console.log(e);
+//     $("#error").html(e.message.replace(/[\n]/g, '<br/>'));
+//   }
+// }
+
+
